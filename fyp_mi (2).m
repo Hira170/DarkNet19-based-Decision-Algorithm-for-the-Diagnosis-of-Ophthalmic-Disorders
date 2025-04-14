2fc15b055f167d1d3fc0f232ff3d36e132d7a155
Hira170 <shahid.hira@outlook.com>
2025-04-14 13:37:44 +0100

    Add files via upload

---
+++ fyp_mi (2).m
@@ -0,0 +1,38 @@
+clc
+clear all
+close all
+
+% numfiles=45;
+% Nfeatures=[];
+% 
+% for k=1:numfiles
+%     I = imread(sprintf('A (%d).jpg', k));
+%     FN=[];
+%     for i=1:3
+%         C=I(:,:,i);
+%         LBP_F=extractLBPFeatures(C);
+%         HOG_F=extractHOGFeatures(C);
+%         F=[LBP_F HOG_F];
+%           FN=[FN F];
+%     end
+%   
+%     Nfeatures = [Nfeatures;FN];
+% end
+%     \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+% T=importdata('raw data.xlsx');
+% F=T.data;
+% X=F';
+% L=[zeros(1,15) ones(1,15) 2*ones(1,15)];
+% [IDX,Z]=rankfeatures(X,L,'Criterion','ttest');
+
+
+
+
+
+
+
+
+
+
+
